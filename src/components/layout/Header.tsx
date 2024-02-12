c35d5028a470478744f7a73aeccba641f45e1541
LeylaKhb <leyla.khbi@gmail.com>
2024-02-12 23:43:54 +0300

    Added menu

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,37 +2,38 @@ import {Link} from "react-router-dom";
 import logo from "../../static/logo.png"
 import React, {useState} from "react";
 import "../../styles/dropdown.scss";
+import MenuHeader from "./MenuHeader";
 
 interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = () => {
-    const [open, setOpen] = useState("");
-    const [hidden, setHidden] = useState(true);
-    function handleSelectTitle() {
-        if (open === "") {
-            setOpen("active");
-            setHidden(false);
-        }
-        if (open === "active") {
-            setHidden(true);
-            setOpen("")
-        }
-    }
+    const [open, setOpen] = useState(false);
 
-    function handleOptionClick() {
-        setOpen("");
-        setHidden(true);
+    function handleMenu() {
+        if (!open) {
+            document.body.style.overflow = "hidden";
+        }
+        else {
+            document.body.style.overflow = "scroll";
+        }
+        setOpen(!open);
     }
 
     return (
         <header>
+            {open && <MenuHeader handleMenuLinks={handleMenu}/>}
+
             <div className="menu_and_logo">
-                <div className="menu_header" >
-                    ☰
+                <div className="menu_header" onClick={handleMenu}>
+                    <span className="menu_header_block" style={{top: 25, display: open ? 'none' : "initial"}}></span>
+                    <span className="menu_header_block" style={{top: 33, transform: open ? "rotate(45deg)" : "none"}}></span>
+                    <span className="menu_header_block" style={{top: 33, transform: open ? "rotate(-45deg)" : "none"}}></span>
+                    <span className="menu_header_block" style={{top: 41, display: open ? 'none' : "initial"}}></span>
                 </div>
 
-                <Link to="/" onClick={handleOptionClick}>
+
+                <Link to="/">
                     <img src={logo} style={{width: '70%'}} alt="emblem"/>
                 </Link>
             </div>
---
+++ src/components/layout/MenuHeader.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+interface MenuHeaderProps {
+    handleMenuLinks: any;
+}
+
+const MenuHeader: React.FC<MenuHeaderProps> = ({handleMenuLinks}) => {
+    return (
+        <div className="menu">
+            <ul role="list" className="menu_ul" onClick={handleMenuLinks}>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/">Главная</a></li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/#about">О нас</a>
+                </li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/#service">Наши услуги</a>
+                </li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/#reviews">Отзывы</a>
+                </li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/#contacts">Контакты</a>
+                </li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/schedule">Расписание поставок</a></li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="https://t.me/+nVp-YvKEbJJjOTRi">Чат в Telegram</a></li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/">Наши партнеры</a></li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/login">Войти</a>
+                </li>
+                <li className="menu_li">
+                    <a className="menu_li_link" href="/registration">Зарегистрироваться</a>
+                </li>
+            </ul>
+        </div>
+    )
+}
+export default MenuHeader;
