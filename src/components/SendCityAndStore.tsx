d0e7b44a7906d9103665e086723f5df8cccb7c78
LeylaKhb <leyla.khbi@gmail.com>
2024-04-03 00:57:08 +0300

    fixed

--- src/components/SendCityAndStore.tsx
+++ src/components/SendCityAndStore.tsx
@@ -24,7 +24,7 @@ const SendCityAndStore: React.FC<SendCityAndStoreProps> = ({handleDepartureCity,
 
     const [cityWildberriesIndex, setCityWildberriesIndex] = useState(0);
     const citiesWildberries = ["Казань", "Невинномысск", "Краснодар", "Чехов 1", "Чехов 2", "Подольск", "Тула",
-        "Коледино", "Электросталь", "Екат (перпективный)", "Екат (индустриальный)", "Новосибирск", "СПб (Уткина заводь)",
+        "Коледино", "Электросталь", "Екат (перпективный)", "Екат (испытателей)", "Новосибирск", "СПб (Уткина заводь)",
         "Новосемейкино", "Преображенка"];
 
     if (location === "admin") {
