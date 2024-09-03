16649d112d19e9e2ef91e8e66c80f4447cebb3f7
LeylaKhb <leyla.khbi@gmail.com>
2024-09-03 15:45:04 +0300

    фикс

--- src/components/personal_page/OrderInfo.tsx
+++ src/components/personal_page/OrderInfo.tsx
@@ -85,13 +85,14 @@ const OrderInfo: React.FC<OrderInfoProps> = ({order, orderPrice, openSecondPopup
               <form method='POST' action='https://ivlev-ff.server.paykeeper.ru/create/'>
                 <input type='hidden' name='client_phone' value={order.phoneNumber}/>
                 <input type='hidden' name='clientid' value={order.entity}/>
-                <input type='hidden' name='sum' value={orderPrice} readOnly
+                <input type='hidden' name='orderid' value={order.id}/>
+                  <input type='hidden' name='sum' value={orderPrice} readOnly
                        onKeyDown={(event) => event.preventDefault()}
                        onPaste={(event) => event.preventDefault()}
                        onCut={(event) => event.preventDefault()}
                        onDragStart={(event) => event.preventDefault()}
                        onDrop={(event) => event.preventDefault()}/>
-                <input type="hidden" name="user_result_callback" value={"https://ivlev-ff.ru/orders/" + order.id}/>
+                <input type="hidden" name="user_result_callback" value={"https://ivlev-ff.ru/personal_account"}/>
                 <input type='hidden' name='service_name'
                        value={'Заказ в ' + order.sendCity + " " + order.store + " от " + order.departureDate}/>
                 <input type='submit' value='Оплатить онлайн' className="change_order"
