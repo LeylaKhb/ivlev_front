21de793342f145898bde98aebfd1c40d21da6e31
LeylaKhb <leyla.khbi@gmail.com>
2025-06-10 09:22:31 +0300

    фикс кэширования

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,16 @@ export const pathResolver = (p: string) => resolve(__dirname, '.', p);
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        manifest: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name].[hash].js',
+                chunkFileNames: 'assets/[name].[hash].js',
+                assetFileNames: 'assets/[name].[hash].[ext]',
+            }
+        }
+    }
 });
